a1243408286da7f169f379cfaf6ad8b33454ed99
cc4rlos <carlos.funes@jpschools.org>
2026-09-22 16:53:30 +0000

    git commit

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -60,7 +60,7 @@ $(function () {
     // TODO 4 - Create Cannons
     createCannon("right",400, 2000)
 
-    createCannon("left",515, 2000)
+    createCannon("left",550, 2000)
 
     createCannon("top", 800, 1300)
     //////////////////////////////////
